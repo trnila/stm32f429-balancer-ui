71de796ebdb6113a391794578276b7f0675ff246
Daniel Trnka <daniel.trnka@gmail.com>
2018-04-27 17:36:22 +0200

    initial commit

---
+++ src/main.c
@@ -0,0 +1,188 @@
+/**
+  ******************************************************************************
+  * @file    Touch_Panel/main.c
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    20-September-2013
+  * @brief   This example describes how to configure and use the touch panel 
+  *          mounted on STM32F429I-DISCO boards.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F429I_DISCOVERY_Examples
+  * @{
+  */
+
+/** @addtogroup Touch_Panel
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void TP_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+
+/**
+  * @brief   Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+  uint16_t linenum = 0;
+  static TP_STATE* TP_State; 
+    
+  /*!< At this stage the microcontroller clock setting is already configured, 
+  this is done through SystemInit() function which is called from startup
+  file (startup_stm32f429_439xx.s) before to branch to application main.
+  To reconfigure the default setting of SystemInit() function, refer to
+  system_stm32f4xx.c file
+  */
+
+
+
+  /* LCD initiatization */
+  LCD_Init();
+  
+  /* LCD Layer initiatization */
+  LCD_LayerInit();
+    
+  /* Enable the LTDC */
+  LTDC_Cmd(ENABLE);
+  
+  /* Set LCD foreground layer */
+  LCD_SetLayer(LCD_FOREGROUND_LAYER);
+  
+  /* Touch Panel configuration */
+  TP_Config();
+
+
+    
+  while (1)
+  {
+ 
+    TP_State = IOE_TP_GetState();
+    
+    if((TP_State->TouchDetected) && ((TP_State->Y < LCD_PIXEL_HEIGHT - 3) && (TP_State->Y >= 3)))
+    {
+      if((TP_State->X >= LCD_PIXEL_WIDTH - 3) || (TP_State->X < 3))
+      {}
+      else
+      {
+    	  LCD_Clear(LCD_COLOR_WHITE);
+        LCD_DrawFullCircle(TP_State->X, TP_State->Y, 10);
+      }
+    }
+  }
+}
+
+/**
+* @brief  Configure the IO Expander and the Touch Panel.
+* @param  None
+* @retval None
+*/
+static void TP_Config(void)
+{
+  /* Clear the LCD */ 
+  LCD_Clear(LCD_COLOR_WHITE);
+  
+  /* Configure the IO Expander */
+  if (IOE_Config() == IOE_OK)
+  {   
+    //LCD_SetFont(&Font8x8);
+
+
+    //LCD_SetBackColor(LCD_COLOR_BLUE2);
+    //LCD_DrawRect(0, 0, LCD_PIXEL_WIDTH, LCD_PIXEL_HEIGHT);
+
+  /*  LCD_DrawFullRect(40, 250, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_YELLOW); 
+    LCD_DrawFullRect(75, 250, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_RED); 
+    LCD_DrawFullRect(5, 288, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_BLUE); 
+    LCD_DrawFullRect(40, 288, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_GREEN); 
+    LCD_DrawFullRect(75, 288, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_MAGENTA); 
+    LCD_DrawFullRect(145, 288, 30, 30);
+    LCD_SetTextColor(LCD_COLOR_BLACK); 
+    LCD_DrawFullRect(110, 288, 30, 30);
+    LCD_DrawRect(180, 270, 48, 50);
+    LCD_SetFont(&Font16x24);
+    LCD_DisplayChar(LCD_LINE_12, 195, 0x43);
+    LCD_DrawLine(0, 248, 240, LCD_DIR_HORIZONTAL);
+    LCD_DrawLine(0, 284, 180, LCD_DIR_HORIZONTAL);
+    LCD_DrawLine(1, 248, 71, LCD_DIR_VERTICAL);
+    LCD_DrawLine(37, 248, 71, LCD_DIR_VERTICAL);
+    LCD_DrawLine(72, 248, 71, LCD_DIR_VERTICAL);
+    LCD_DrawLine(107, 248, 71, LCD_DIR_VERTICAL);
+    LCD_DrawLine(142, 284, 36, LCD_DIR_VERTICAL);
+    LCD_DrawLine(0, 319, 240, LCD_DIR_HORIZONTAL);
+    */
+  }  
+  else
+  {
+    LCD_Clear(LCD_COLOR_RED);
+    LCD_SetTextColor(LCD_COLOR_BLACK); 
+    LCD_DisplayStringLine(LCD_LINE_6,(uint8_t*)"   IOE NOT OK      ");
+    LCD_DisplayStringLine(LCD_LINE_7,(uint8_t*)"Reset the board   ");
+    LCD_DisplayStringLine(LCD_LINE_8,(uint8_t*)"and try again     ");
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
