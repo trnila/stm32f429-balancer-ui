0fb3f6feb959b8e854591a78942f43f89284dc84
Daniel Trnka <daniel.trnka@gmail.com>
2018-04-28 13:42:06 +0200

    Refactored uart.c

--- src/uart.c
+++ src/uart.c
@@ -1,17 +1,22 @@
 #include "stm32f4xx.h"
 #include "uart.h"
 
+const GPIO_TypeDef *uart_gpio = GPIOG;
+const USART_TypeDef *usart = USART6;
+
 uint8_t buffer[BUFFER_MAX];
 int pos = 0;
 
-int x = 0, y = 0;
 QueueHandle_t rx_queue;
 
 
 void uart_send(const uint8_t *data, int size) {
 	while(size--) {
-		while( !(USART6->SR & 0x00000040) );
-		USART_SendData(USART6, *data);
+		// wait for transmission complete
+		while(!(usart->SR & USART_FLAG_TC));
+
+		// send next byte
+		USART_SendData(usart, *data);
 		data++;
 	}
 }
@@ -22,72 +27,63 @@ void ctrl_sendtarget(int x, int y) {
 }
 
 void uart_init() {
-	USART_InitTypeDef usart1_init_struct;
-	GPIO_InitTypeDef gpioa_init_struct;
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	// enable clocks for gpio and uart peripheral
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
-
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
 
 
-	/* GPIOA PIN9 alternative function Tx */
+	// configure gpio
+	GPIO_InitTypeDef gpioa_init_struct;
 	gpioa_init_struct.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_14;
 	gpioa_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
 	gpioa_init_struct.GPIO_Mode = GPIO_Mode_AF;
 	gpioa_init_struct.GPIO_PuPd = GPIO_PuPd_UP;
 	gpioa_init_struct.GPIO_OType = GPIO_OType_PP;
-	GPIO_Init(GPIOG, &gpioa_init_struct);
+	GPIO_Init(uart_gpio, &gpioa_init_struct);
 
-	GPIO_PinAFConfig(GPIOG, GPIO_PinSource9, GPIO_AF_USART6);
-		GPIO_PinAFConfig(GPIOG, GPIO_PinSource14, GPIO_AF_USART6);
+	// configure alternate functions to uart
+	GPIO_PinAFConfig(uart_gpio, GPIO_PinSource9, GPIO_AF_USART6);
+	GPIO_PinAFConfig(uart_gpio, GPIO_PinSource14, GPIO_AF_USART6);
 
+
+
+	// configure uart peripheral
+	USART_InitTypeDef usart1_init_struct;
 	usart1_init_struct.USART_BaudRate = 9600;
 	usart1_init_struct.USART_WordLength = USART_WordLength_8b;
 	usart1_init_struct.USART_StopBits = USART_StopBits_1;
 	usart1_init_struct.USART_Parity = USART_Parity_No ;
 	usart1_init_struct.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 	usart1_init_struct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	/* Configure USART2 */
-	USART_Init(USART6, &usart1_init_struct);
+	USART_Init(usart, &usart1_init_struct);
 
-	/* Enable RXNE interrupt */
-
-	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
-	NVIC_InitStructure.NVIC_IRQChannel = USART6_IRQn;		 // we want to configure the USART1 interrupts
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;// this sets the priority group of the USART1 interrupts
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;		 // this sets the subpriority inside the group
-	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			 // the USART1 interrupts are globally enabled
+	// prepare uart rx interrupts
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART6_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 4;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 
 	NVIC_SetPriority(USART6_IRQn, 15);
-
-	USART_ITConfig(USART6, USART_IT_RXNE, ENABLE);
-
-	/* Enable USART2 global interrupt */
+	USART_ITConfig(usart, USART_IT_RXNE, ENABLE);
 	NVIC_EnableIRQ(USART6_IRQn);
 
-
-	USART_Cmd(USART6, ENABLE);
+	// finally enable uart
+	USART_Cmd(usart, ENABLE);
 }
 
 void USART6_IRQHandler() {
 	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-	volatile int ret = 111;
 
-	if( USART_GetITStatus(USART6, USART_IT_RXNE) ){
-		buffer[pos] = USART6->DR;
+	// is received?
+	if(USART_GetITStatus(usart, USART_IT_RXNE)){
+		// get byte and clear interrupt flag
+		buffer[pos] = usart->DR;
 
 		if(pos >= 1) {
-			//x = buffer[0];
-			//y = buffer[1];
 			pos = 0;
-
-			ret = xQueueSendFromISR(rx_queue, buffer, &xHigherPriorityTaskWoken);
+			xQueueSendFromISR(rx_queue, buffer, &xHigherPriorityTaskWoken);
 		} else {
 			pos = (pos + 1) % sizeof(buffer);
 		}
